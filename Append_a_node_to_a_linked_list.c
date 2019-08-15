1a3da881e40f5ec947266d23be8e34560eb1cf9e
Abomination1 <rozic.dejan@gmail.com>
2019-08-15 18:51:34 +0200

    Create Append_a_node_to_a_linked_list.c

---
+++ Append_a_node_to_a_linked_list.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct digit {
+    int num;
+    struct digit * next;
+};
+struct digit * createDigit(int dig);
+struct digit * append(struct digit * end, struct digit * newDigitptr);
+int main(void) {
+    //! stack=showMemory(start=65520,cursors=[start,newDigitptr,end,tmp])
+    struct digit *start, *newDigitptr, *end;
+    int first = 5;
+    int second = 3;
+    int third = 7;
+    start = createDigit(first);
+    end = start;
+    newDigitptr = createDigit(second);
+    end = append(end, newDigitptr);
+    newDigitptr = createDigit(third);
+    end = append(end, newDigitptr);
+    // free needs to be added  here
+    return 0;
+}
+
+struct digit * append(struct digit * end, struct digit * newDigitptr) {
+    //! heap=showMemory(start=260, cursors=[end,newDigitptr])
+    end->next = newDigitptr;
+    end = newDigitptr;
+    return(end);
+}
+
+struct digit * createDigit(int dig) {
+    //! heap=showMemory(start=260, cursors=[ptr])
+    struct digit *ptr;
+    ptr = (struct digit *) malloc(sizeof(struct digit));
+    ptr->num = dig;
+    ptr->next = NULL;
+    return ptr;
+}
